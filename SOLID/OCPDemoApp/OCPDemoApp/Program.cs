d54de582241b4c9708a209476631f47473af9c48
Mikhail Holubeu <mihanekmiha3105@gmail.com>
2022-06-19 14:27:29 +0300

    Add initial app without OCP

---
+++ SOLID/OCPDemoApp/OCPDemoApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using OCPLibrary;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var applicants = new List<Person>()
+            {
+                new Person { FirstName = "Mike", LastName = "Johnson" },
+                new Person { FirstName = "Vito", LastName = "Cruise" },
+                new Person { FirstName = "Tim", LastName = "Storm" }
+            };
+
+            var employees = new List<Employee>();
+            var accountGenerator = new AccountGenerator();
+
+            foreach (var person in applicants)
+            {
+                employees.Add(accountGenerator.Create(person));
+            }
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}: {employee.Email}");
+            }
+        }
+    }
+}
---
+++ SOLID/OCPDemoApp/OCPLibrary/AccountGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace OCPLibrary
+{
+    public class AccountGenerator
+    {
+        public Employee Create(Person person)
+        {
+            var result = new Employee();
+
+            result.FirstName = person.FirstName;
+            result.LastName = person.LastName;
+            result.Email = $"{person.FirstName.Substring(0, 1).ToLower()}{person.LastName.ToLower()}@gmail.com";
+
+            return result;
+        }
+    }
+}
