dd4db72081b0cf639cf62add52a1d8a8ec370b17
Mikhail Holubeu <mihanekmiha3105@gmail.com>
2022-06-20 13:12:01 +0300

    Add demo app without ISP

---
+++ SOLID/ISPDemoApp/ISPLibrary/Interfaces/ILibraryItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ISPLibrary.Interfaces
+{
+    public interface ILibraryItem
+    {
+        string LibraryId { get; set; }
+        string Title { get; set; }
+        string Author { get; set; }
+        int Pages { get; set; }
+        int CheckOutDurationInDays { get; set; }
+        string Borrower { get; set; }
+        DateTime BorrowDate { get; set; }
+
+        void CheckOut(string borrower);
+        void CheckIn();
+        DateTime GetDueDate();
+    }
+}
