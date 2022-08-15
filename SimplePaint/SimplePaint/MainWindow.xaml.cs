ab66c7e2fbcc43a5d3c5846495f916ce5d732843
Mikhail Holubeu <mihanekmiha3105@gmail.com>
2022-08-15 12:42:58 +0300

    Scaffold the project, created the UI

---
+++ SimplePaint/SimplePaint/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace SimplePaint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private enum Sizes
+        {
+            Small = 10,
+            Medium = 20,
+            Large = 30
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
