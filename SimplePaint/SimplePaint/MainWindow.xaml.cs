bf282b1b8ebef4674b89239435f7657047991db9
Mikhail Holubeu <mihanekmiha3105@gmail.com>
2022-08-15 15:47:20 +0300

    Add window initializing

--- SimplePaint/SimplePaint/MainWindow.xaml.cs
+++ SimplePaint/SimplePaint/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows;
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Color = System.Drawing.Color;
+using Pen = System.Drawing.Pen;
+using Point = System.Windows.Point;
 
 namespace SimplePaint
 {
@@ -7,15 +14,25 @@ namespace SimplePaint
     /// </summary>
     public partial class MainWindow : Window
     {
-        private enum Sizes
-        {
-            Small = 10,
-            Medium = 20,
-            Large = 30
-        }
+        private Bitmap _bitmap;
+        private Graphics _graphics;
+        Point _pointX, _pointY;
+        private Pen _pen = new Pen(Color.Black, 1);
+
+        private bool _isDrawing = false;
+        private bool _isErasing = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            _bitmap = new Bitmap((int)Picture.Width, (int)Picture.Height);
+            _graphics = Graphics.FromImage(_bitmap);
+            _graphics.Clear(Color.White);
+        }
+
+        private void Canvas_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
         }
     }
 }
